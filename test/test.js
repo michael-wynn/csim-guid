d4826f9fd330ad1a71f45a36cecacecaf9ed83c4
michael-wynn <vrcmwynn@hotmail.com>
2015-07-30 08:02:21 -0700

    Implement counter reseed on timestamp change

--- test/test.js
+++ test/test.js
@@ -34,17 +34,22 @@ describe('csim-guid tests', function(){
         });
 
         it('Timestamp value to match present', function(){
-            assert(Math.abs(breakdown1.timeStamp - now) <= 100, 'Timestamp too different from now');
-            assert(Math.abs(breakdown2.timeStamp - now) <= 100, 'Timestamp too different from now');
+            assert(Math.abs(breakdown1.timestamp - now) <= 100, 'Timestamp too different from now');
+            assert(Math.abs(breakdown2.timestamp - now) <= 100, 'Timestamp too different from now');
         });
 
         it('Timestamp should increase successively', function(){
-            assert(breakdown2.timeStamp >= breakdown1.timeStamp, '2nd timestamp not greater than 1st');
+            assert(breakdown2.timestamp >= breakdown1.timestamp, '2nd timestamp less than 1st');
         });
 
-        it('Timestamp on successive item should be close', function(){
-            assert(Math.abs(breakdown1.timeStamp - breakdown2.timeStamp) < 100,
-                'Successive timestamps too different');
+        it('Timestamp on later item should be greater', function(done){
+            var resolution = 1; //as little as 1 millisecond should have effect
+            setTimeout(function(){
+                var newGuid = g();
+                var newBreakdown = g.breakdown(newGuid);
+                assert(newBreakdown.timestamp > breakdown1.timestamp);
+                done();
+            }, resolution);
         });
 
         it('Counter values should be successive or reset', function(){
@@ -63,13 +68,24 @@ describe('csim-guid tests', function(){
     describe('Counter roll-over verification', function(){
 
         before(function(){
-            g.setCounterValue(counterMax); //set counter at maximum to trigger roll-over
-            guid1 = g();
-            breakdown1 = g.breakdown(guid1);
-            guid2 = g();
-            breakdown2 = g.breakdown(guid2);
-            guid3 = g();
-            breakdown3 = g.breakdown(guid3);
+            // attempt to generate 3 guids on same timestamp at artificially high
+            // counter, to trigger counter rollover for testing the rollover
+            // algorithm.  Note that due to reseeding plan, the chance of actual
+            // rollover in real application is very slim.
+            do {
+                //dummy call to reduce probability of counter reseed on next call
+                var guid0 = g();
+                //set counter at maximum to trigger roll-over on guid2 creation
+                g.setCounterValue(counterMax);
+                guid1 = g();
+                guid2 = g();
+                guid3 = g();
+
+                var breakdown0 = g.breakdown(guid0);
+                breakdown1 = g.breakdown(guid1);
+                breakdown2 = g.breakdown(guid2);
+                breakdown3 = g.breakdown(guid3);
+            } while(breakdown1.timestamp.getTime() != breakdown0.timestamp.getTime());
         });
 
         it('Counter value expectation: max', function(){
@@ -78,13 +94,13 @@ describe('csim-guid tests', function(){
         });
 
         it('Counter value expectation: after rollover', function(){
-            assert(breakdown2.counter === 0, 'Counter not at 0');
-            assert(guid2.length === 25, 'Default guid not 25 chars as expected');
+            assert(breakdown2.counter < breakdown1.counter, 'Counter not reseeded');
+            assert(guid2.length === 25, 'Default guid not 25 chars');
         });
 
-        it('Counter value expectation: 1', function(){
-            assert(breakdown3.counter === 1, 'Counter not at 1');
-            assert(guid3.length === 25, 'Default guid not 25 chars as expected');
+        it('Counter value expectation: normal', function(){
+            assert(breakdown3.counter === breakdown2.counter+1, 'Counter not sequential');
+            assert(guid3.length === 25, 'Default guid not 25 chars');
         });
 
     });
